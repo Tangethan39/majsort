51d252716f50e96a47bb5bf494047f1a35c4ed82
Tangethan39 <72875233+Tangethan39@users.noreply.github.com>
2022-02-25 20:29:25 -0500

    Add files via upload

---
+++ src/js/data/2018-02-22.js
@@ -0,0 +1,462 @@
+dataSetVersion = "2022-02-23"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Gender",
+    key: "gender",
+    tooltip: "Check this to restrict to certain gender.",
+    checked: false,
+    sub: [
+      { name: "Female", key: "female" },
+      { name: "Male", key: "male" }
+    ]
+  },
+  {
+   name: "Loli/Shota",
+   key: "loli",
+   tooltip: "Check this to restrict children.",
+   checked: false,
+   sub: [
+      { name: "not loli/shota", key: "nloli"},
+      { name: "Loli", key: "loli" }, 
+      { name: "Shota", key: "shota"}
+    ]
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Ichihime",
+    img: "5MEhtUz.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+    },
+  },
+  {
+    name: "Miki Nikaidou",
+    img: "KfNuUG9.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+    },
+  },
+  {
+    // chioriwow
+    name: "Chiori Mikami",
+    img: "ezq0LjI.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Kana Fujita",
+    img: "RZE8L8m.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+    },
+  },
+  {
+    name: "Mai Aihara",
+    img: "tSQfGao.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+    },
+  },
+  {
+    name: "Nadeshiko",
+    img: "6PAZCmt.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Yui Yagi",
+    img: "vZ9AjjQ.png",
+    opts: {
+      gender: ['female' ],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Riu Kujou",
+    img: "lfnvFXl.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Xenia",
+    img: "hUu2ML6.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Kaavi",
+    img: "cvCngET.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Sara",
+    img: "3P3xV0F.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Hana Minomiya",
+    img: "lSzQEQV.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Keikumusume",
+    img: "z24i6Ts.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Nana Shiraishi",
+    img: "M467n0q.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Hinata Takanashi",
+    img: "Ir9WBUU.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Haruna igarashi",
+    img: "TpXhu7e.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli'],
+   },
+  },
+  {
+    name: "Anju Suzumiya",
+    img: "0eyLNrZ.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Sawako Kitami",
+    img: "6HxajEY.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Momo Hina",
+    img: "iDKrm6G.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Kaguyahime",
+    img: "coyNInC.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Kirara Fujimoto",
+    img: "z4uQBJI.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Eliisa",
+    img: "t6QLMFK.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Chihori Terasaki",
+    img: "XRiQenT.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Saki Miyanaga",
+    img: "Hh8h0SL.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Nodoka Haramura",
+    img: "wvX46cw.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Koromo Amae",
+    img: "mgdnGtQ.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "teru Miyanaga",
+    img : "DLK3AOE.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Fu Ji",
+    img: "448gEB6.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Yumeko Jibami",
+    img: "Ud6bEs3.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Mary Saotome",
+    img: "vSgDGp5.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Midari Ikishima",
+    img: "DL1XZLF.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Kirari Momobami",
+    img: "fofZxWk.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Reina Nanami",
+    img: "jXN5Vxx.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Hibiki Himekawa",
+    img: "3ELB59f.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Ayako Morikawa",
+    img: "y6wTjsA.png",
+    opts: {
+      gender: ['female'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Kazuha Saionji",
+    img: "amsFj9o.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Nanaha Onodera",
+    img: "Sh5L8VX.png",
+    opts: {
+      gender: ['female'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Wanjirou",
+    img: "B0geMC8.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Natsuki Shinomiya",
+    img: "j12ImR2.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Sora Ichinose",
+    img: "dM5xQbo.png",
+    opts: {
+      gender: ['male'],
+      loli: ['shota']
+   },
+  },
+  {
+    name: "Hideki Akechi",
+    img: "6mKvxx4.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Osamu Saitou",
+    img: "kTE7ntW.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Jospeh",
+    img: "a6vEnNe.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Ein",
+    img: "WgG4COa.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+   {
+    name: "Zan Tsukimi",
+    img: "DsLmgqN.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Ren Kisaragi",
+    img: "ii2WXKa.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Usumi Ishihara",
+    img: "QDGYACZ.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Qi Xi",
+    img: "ILUvtoB.png",
+    opts: {
+      gender: ['male'],
+      loli: ['shota']
+   },
+  },
+  {
+    name: "A-37",
+    img: "LZVTLAJ.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Ryan",
+    img: "NWjqHz8.png",
+    opts: {
+      gender: ['male'],
+      loli: ['loli']
+   },
+  },
+  {
+    name: "Natsuhiko Takigawa",
+    img: "NLtGe7H.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Shigeru Akagi",
+    img: "jbNKX3V.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Iwao Washizu",
+    img: "jPqbZI3.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+  {
+    name: "Sammir",
+    img: "pGoG6Ga.png",
+    opts: {
+      gender: ['male'],
+      loli: ['nloli']
+   },
+  },
+];
